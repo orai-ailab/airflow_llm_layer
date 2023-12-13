306951417076c81a4153a2cd34788289a90dcf8b
hoangks5 <hoangnd.ute@gmail.com>
2023-12-13 14:55:12 +0700

    update

--- all_coin_info.py
+++ all_coin_info.py
@@ -48,8 +48,8 @@ def update_data_to_mongodb(**kwargs):
     db = client["LLM_DataLake"]
     collection = db['Coingecko_allcoin']        
     collection.insert_many(coin_data)
-    # update upper case symbol
-    #collection.update_many({}, [{"$set": {"symbol": {"$toUpper": "$symbol"}}}])
+    
+    collection.update_many({}, [{"$set": {"symbol": {"$toUpper": "$symbol"}}}])
     print("All data saved to MongoDB")
     
 dag = DAG('All_coingecko_info', default_args=default_args, schedule_interval='@daily')
