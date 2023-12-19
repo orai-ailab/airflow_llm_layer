b001c6137f9023956ba6877c62add1482008331e
iamnvh <nguyenvanhao.ktpm02@gmail.com>
2023-12-19 14:58:34 +0700

    test elastic

--- test/test.dag.py
+++ test/test.dag.py
@@ -1,9 +1,20 @@
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airfow_git.lunarcrush.elasticsearch_service import connect, create_or_update, check_or_create_index
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+es_username = os.environ.get('ES_NAME')
+es_password = os.environ.get('ES_PASSWORD')
+es_host = os.environ.get('ES_HOST')
+es_port = os.environ.get("ES_PORT")
 
-from datetime import datetime
+# init elastic
+client = connect(es_username, es_password, es_host, es_port)
+index_name = 'lunarcrush-coin-info'
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -11,7 +22,7 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id='test',
+    dag_id='test_eslasticsearch',
     default_args=default_args,
     schedule_interval='@once',
     catchup=False,
@@ -20,7 +31,8 @@ dag = DAG(
 
 
 def test():
-    print(1)
+    check_or_create_index(index_name, client)
+    print("Done")
 
 
 t1 = PythonOperator(
